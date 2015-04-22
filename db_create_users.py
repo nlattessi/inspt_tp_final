7c35b98a9de8854ede5c31cb4bfea1380bfe7539
nlattessi <n_lattessi@hotmail.com>
2015-04-22 02:40:00 -0300

    borrado archivos viejos

--- db_create_users.py
+++
@@ -1,9 +0,0 @@
-from project import db
-from project.models import User
-
-# Inserto data
-db.session.add(User("admin", "admin@admin.com", "admin"))
-db.session.add(User("nltattessi", "nltattessi@gmail.com", "nahlatk2409"))
-
-# Commiteo
-db.session.commit()
--- sql.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("sample.db") as connecton:
-    c = connecton.cursor()
-    c.execute("DROP TABLE menu_items")
-    c.execute("CREATE TABLE menu_items(nombre TEXT, precio INTEGER)")
-    c.execute('INSERT INTO menu_items VALUES("hamburgesa con queso", 30)')
-    c.execute('INSERT INTO menu_items VALUES("ensalada caprese", 45.50)')
